91ce4de37bf5b1434f804365f69e3e9644abc0b8
art2611 <art261197@hotmail.fr>
2020-11-26 11:45:55 +0100

    k

--- model.py
+++ model.py
@@ -35,8 +35,6 @@ class visible_module(nn.Module):
         x = self.visible.bn1(x)
         x = self.visible.relu(x)
         x = self.visible.maxpool(x)
-        x = self.visible.layer1(x)
-        x = self.visible.layer2(x)
         return x
 
 class thermal_module(nn.Module):
@@ -52,8 +50,6 @@ class thermal_module(nn.Module):
         x = self.thermal.bn1(x)
         x = self.thermal.relu(x)
         x = self.thermal.maxpool(x)
-        x = self.thermal.layer1(x)
-        x = self.thermal.layer2(x)
         return x
 
 class shared_resnet(nn.Module):
@@ -67,7 +63,8 @@ class shared_resnet(nn.Module):
         self.base = model_base
 
     def forward(self, x):
-
+        x = self.base.layer1(x)
+        x = self.base.layer2(x)
         x = self.base.layer3(x)
         x = self.base.layer4(x)
         return x
@@ -92,11 +89,11 @@ class Network(nn.Module):
 
     def forward(self, x1, x2, modal=0):
         if modal == 0:
-            x1 = self.visible_module(x1)    # Early : torch.Size([32, 64, 72, 36])  Middle :   End : torch.Size([32, 2048, 9, 5])
+            x1 = self.visible_module(x1)    # Early : torch.Size([32, 64, 72, 36])  Middle : ([32, 512, 36, 18])  End : torch.Size([32, 2048, 9, 5])
             x2 = self.thermal_module(x2)
-            x = torch.cat((x1, x2), 0)
-            print(x1.shape)
-            print(x.shape)
+            x = torch.cat((x1, x2), 0)      # Early : torch.Size([64, 64, 72, 36])  Middle : ([64, 512, 36, 18])  End : torch.Size([64, 2048, 9, 5])
+            # print(x1.shape)
+            # print(x.shape)
         elif modal == 1:
             x = self.visible_module(x1)
         elif modal == 2:
