cea4c1edf0b73f6ff101af4465b1d363334f0580
art2611 <art261197@hotmail.fr>
2020-11-26 14:44:09 +0100

    k

--- data_loader.py
+++ data_loader.py
@@ -64,7 +64,7 @@ class RegDBData(data.Dataset):
 
 class SYSUData(data.Dataset):
     def __init__(self, data_dir, transform=None, colorIndex=None, thermalIndex=None):
-        data_dir = '../Datasets/SYSU-MM01/'
+        data_dir = '../Datasets/SYSU/'
         # Load training images (path) and labels
         train_color_image = np.load(data_dir + 'train_rgb_resized_img.npy')
         self.train_color_label = np.load(data_dir + 'train_rgb_resized_label.npy')
