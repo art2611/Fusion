01743ba7375a4d4c9a041fec9fda1d8d2b100c80
art2611 <art261197@hotmail.fr>
2020-11-29 21:30:15 +0100

    k

--- data_loader.py
+++ data_loader.py
@@ -170,7 +170,7 @@ def process_query_sysu(data_path, trial=0, mode='all', relabel=False):
         query_img.append(img_path)
         query_id.append(pid)
         query_cam.append(camid)
-    print(query_id)
+    # print(query_id)
     #print(query_img)
     return query_img, np.array(query_id), np.array(query_cam)
 
