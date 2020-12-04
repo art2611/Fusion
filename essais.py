081a191d959010181b5b9c0d8791916826d3e3cc
art2611 <art261197@hotmail.fr>
2020-12-04 10:46:44 +0100

    k

--- essais.py
+++ essais.py
@@ -2,9 +2,9 @@ import numpy as np
 
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+
 
 x = np.arange(0, 5, 0.1)
 y = np.sin(x)
-plt.plot(x, y)
-plt.show()
+matplotlib.pyplot.plot(x, y)
+matplotlib.pyplot.show()
