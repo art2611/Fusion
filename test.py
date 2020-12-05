027722cb92ae2b67f2e08af2e1520aba0a6b6f74
art2611 <art261197@hotmail.fr>
2020-12-05 12:05:12 +0100

    k

--- test.py
+++ test.py
@@ -63,7 +63,6 @@ elif args.dataset == 'regdb':
     nclass = 206
     data_path = '../Datasets/RegDB/'
     suffix = f'RegDB_{args.reid}_person_fusion({num_of_same_id_in_batch})_same_id({batch_num_identities})_lr_{lr}'
-    suffix = 'RegDB_person_fusion(4)_same_id(8)_lr_0.001'
 
 def extract_gall_feat(gall_loader, ngall, net):
     net.eval()
