57be9b10aabdb77b81b9633f4c9b7fb786a0342a
art2611 <art261197@hotmail.fr>
2020-12-09 14:05:39 +0100

    k

--- test.py
+++ test.py
@@ -124,7 +124,7 @@ def extract_query_feat(query_loader, nquery, net):
         print(f"Query test on mode {test_mode} supposed to be 1 if visible or 2 if thermal" )
         with torch.no_grad():
             for batch_idx, (input1, input2, label) in enumerate(query_loader):
-                batch_num = input1.size(0)
+                batch_num = input1.size(0)*2
                 input1 = Variable(input1.cuda())
                 input2 = Variable(input2.cuda())
                 feat_pool, feat_fc = net(input1, input2, modal=test_mode)
@@ -171,13 +171,13 @@ def multi_process() :
             if args.reid == "BtoB" :
                 query_img, query_img_t, query_label, gall_img, gall_img_t, gall_label = process_test_regdb(data_path, trial=test_trial, modal=args.reid, split=args.split)
                 gallset = TestData_both(gall_img, gall_img_t, gall_label, transform=transform_test, img_size=(img_w, img_h))
-                gall_loader = torch.utils.data.DataLoader(gallset, batch_size=int(test_batch_size/2), shuffle=False,
+                gall_loader = torch.utils.data.DataLoader(gallset, batch_size=int(test_batch_size), shuffle=False,
                                                           num_workers=workers)
                 nquery = len(query_label)
                 ngall = len(gall_label)
 
                 queryset = TestData_both(query_img, query_img_t, query_label, transform=transform_test, img_size=(img_w, img_h))
-                query_loader = torch.utils.data.DataLoader(queryset, batch_size=int(test_batch_size/2), shuffle=False,
+                query_loader = torch.utils.data.DataLoader(queryset, batch_size=int(test_batch_size), shuffle=False,
                                                            num_workers=4)
 
             else :
