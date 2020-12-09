5a8673b0e5183c502217f299afd65d52462f874f
art2611 <art261197@hotmail.fr>
2020-12-09 14:53:12 +0100

    k

--- test.py
+++ test.py
@@ -184,13 +184,13 @@ def multi_process() :
             if args.reid == "BtoB" :
                 query_img, query_img_t, query_label, gall_img, gall_img_t, gall_label = process_test_regdb(data_path, trial=test_trial, modal=args.reid, split=args.split)
                 gallset = TestData_both(gall_img, gall_img_t, gall_label, transform=transform_test, img_size=(img_w, img_h))
-                gall_loader = torch.utils.data.DataLoader(gallset, batch_size=int(test_batch_size), shuffle=False,
+                gall_loader = torch.utils.data.DataLoader(gallset, batch_size=int(test_batch_size/2), shuffle=False,
                                                           num_workers=workers, drop_last=False)
                 nquery = len(query_label)
                 ngall = len(gall_label)
 
                 queryset = TestData_both(query_img, query_img_t, query_label, transform=transform_test, img_size=(img_w, img_h))
-                query_loader = torch.utils.data.DataLoader(queryset, batch_size=int(test_batch_size), shuffle=False,
+                query_loader = torch.utils.data.DataLoader(queryset, batch_size=int(test_batch_size/2), shuffle=False,
                                                            num_workers=4, drop_last=False)
 
             else :
