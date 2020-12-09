c546e84a34d71a6ff8ea5e7bf2ea9c27da8b5398
art2611 <art261197@hotmail.fr>
2020-12-09 14:30:14 +0100

    k

--- test.py
+++ test.py
@@ -135,9 +135,10 @@ def extract_query_feat(query_loader, nquery, net):
                 input1 = Variable(input1.cuda())
                 input2 = Variable(input2.cuda())
                 feat_pool, feat_fc = net(input1, input2, modal=test_mode)
-
+                print(feat_pool.shape)
                 query_feat_pool[ptr:ptr + batch_num, :] = feat_pool.detach().cpu().numpy()
                 query_feat_fc[ptr:ptr + batch_num, :] = feat_fc.detach().cpu().numpy()
+                print(query_feat_pool)
                 ptr = ptr + batch_num
         print('Extracting Time:\t {:.3f}'.format(time.time() - start))
     else :
